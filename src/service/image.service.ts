9de503df263efe8baf39d1cfe2216779b6e2da53
mrslimslim <2451319596@qq.com>
2023-11-18 21:19:34 +0800

    update

--- src/service/image.service.ts
+++ src/service/image.service.ts
@@ -2,16 +2,22 @@ import { Provide } from '@midwayjs/core';
 // read from prisma
 import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
 @Provide()
 export class ImageService {
   async getRandomImage() {
-    // random read a item from prisma
-    const prisma = new PrismaClient();
-    const image = await prisma.image.findFirst({
-      orderBy: {
-        id: 'desc',
-      },
+    // 随机读取db中的一条数据
+
+    // 随机选择1条数据
+    // count of images from DB
+    const count = await prisma.image.count();
+
+    const randomImage = await prisma.image.findFirst({
+      take: 1,
+      skip: Math.floor(Math.random() * count),
     });
-    return image;
+    console.log(randomImage);
+    return randomImage;
   }
 }
